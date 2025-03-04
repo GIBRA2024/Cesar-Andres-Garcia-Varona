c1a12d72160c87ef7b1161d80b1cff9a937b9901
Mateo <monorymateo1@gmail.com>
2025-03-04 12:06:53 -0500

    cambios

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function descargarContacto() {
     const vcardData = `BEGIN:VCARD
 VERSION:3.0
-N:Jimenez Arana;Javier;;;
-FN:Javier Jimenez Arana
-TEL:+57 315 5843205
-EMAIL;TYPE=WORK: gerencia@cervalle.com
-ADR;TYPE=WORK:;;Cra. 29b #11-90;Acopi;Yumbo;;Colombia
+N:García Varona;Cesar;Andrés;;
+FN:Cesar Andrés García Varona
+TEL:+57 3102776017
+EMAIL;TYPE=WORK: directorporcicultura@cervalle.com
 END:VCARD`;
 
     const blob = new Blob([vcardData], { type: 'text/vcard' });
